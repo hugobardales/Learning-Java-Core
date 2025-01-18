8c5937f1b3678a015f9aa5424725b6740adecc21
hugobardales <hboy_go@hotmail.com>
2025-01-18 10:22:36 -0500

    BizzFuzz with work flow for

---
+++ JavaWork/For/.vscode/FizzBuzzLoop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FizzBuzzLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt(), num2 = scanner.nextInt();
+        
+        for (int i = num1; i <= num2; i++) {
+
+            if ((i % 3 == 00) && (i % 5 == 00)) {
+                System.out.println("FizzBuzz");
+            } else if ((i % 3 == 00)) {
+                System.out.println("Fizz");
+            } else if ((i % 5 == 00)) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            } 
+        }
+        scanner.close();
+    }
+}
